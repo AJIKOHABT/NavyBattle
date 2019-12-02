96c5ad77927d974d1bbf000721e9b6c8fa8d26a0
AJIKOHABT <AJIKOHABT@yandex.ru>
2019-12-02 22:23:06 +0400

    1. Game - class for the navybattle game 2. GameBattleShip - class for battleship in the navybattle game

---
+++ NavyBattleModels/Models/Game.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NavyBattleModels.Interfaces;
+
+namespace NavyBattleModels.Models
+{
+    /// <summary>
+    /// Class for the game
+    /// </summary>
+    public class Game: IGame
+    {
+        #region properties and fields
+
+        /// <summary>
+        /// Game id
+        /// </summary>
+        private int _id;
+
+        /// <summary>
+        /// Battlefield on which the game will take place
+        /// </summary>
+        private IBattleField _battleField;
+
+        /// <summary>
+        /// BattleShips in the game
+        /// </summary>
+        private IEnumerable<IGameBattleShip> _gameBattleShips = new List<IGameBattleShip>();
+
+        /// <summary>
+        /// Shots that were made during the game
+        /// </summary>
+        private IEnumerable<IShot> _shots = new List<IShot>();
+
+        /// <summary>
+        /// Game id
+        /// </summary>
+        public int Id
+        {
+            get 
+            {
+                return _id;
+            }
+        }
+
+        /// <summary>
+        /// Battlefield on which the game will take place
+        /// </summary>
+        public IBattleField BattleField
+        {
+            get
+            {
+                return _battleField;
+            }
+        }
+
+        /// <summary>
+        /// BattleShips in the game
+        /// </summary>
+        public IEnumerable<IGameBattleShip> GameBattleShips
+        {
+            get 
+            {
+                return _gameBattleShips;
+            }
+        }
+
+        /// <summary>
+        /// Shots that were made during the game
+        /// </summary>
+        public IEnumerable<IShot> Shots
+        {
+            get
+            {
+                return _shots;
+            }
+        }
+        #endregion
+
+        #region constructor
+
+        /// <summary>
+        /// Constructor of the game
+        /// </summary>
+        /// <param name="battleField">Battlefield on which the game will take place</param>
+        public Game(IBattleField battleField)
+        {
+            _battleField = battleField;
+            GenerateGameBattleShips(battleField);
+        }
+
+        #endregion
+
+        #region private methods
+
+        /// <summary>
+        /// Generating battleships for the game
+        /// </summary>
+        /// <param name="battleField">Battlefield which battleships will be used in the game</param>
+        private void GenerateGameBattleShips(IBattleField battleField)
+        {            
+            foreach (var battleShip in battleField.Battleships)
+            {
+                _gameBattleShips.Add(new GameBattleShip(this, battleShip));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ NavyBattleModels/Models/GameBattleShip.cs
@@ -0,0 +1,79 @@
+﻿using NavyBattleModels.Interfaces;
+
+namespace NavyBattleModels.Models
+{
+    /// <summary>
+    /// Class for battleship in the game
+    /// </summary>
+    public class GameBattleShip : IGameBattleShip
+    {
+        #region properties and fields
+
+        /// <summary>
+        /// Game in which battleship stands
+        /// </summary>
+        private IGame _game;
+
+        /// <summary>
+        /// Battleship on the battleField
+        /// </summary>
+        private IBattleShip _battleShip;
+
+        /// <summary>
+        /// State of the battleship in the game
+        /// </summary>
+        private BattleShipState _state;
+
+        /// <summary>
+        /// Game in which battleship stands
+        /// </summary>
+        public IGame Game
+        {
+            get 
+            {
+                return _game;
+            }
+        }
+
+        /// <summary>
+        /// Battleship on the battleField
+        /// </summary>
+        public IBattleShip BattleShip
+        {
+            get 
+            {
+                return _battleShip;
+            }
+        }
+
+        /// <summary>
+        /// State of the battleship in the game
+        /// </summary>
+        public BattleShipState State
+        {
+            get
+            {
+                return _state;
+            }
+        }
+
+        #endregion
+
+        #region constructor
+
+        /// <summary>
+        /// Constructor of the battleship in the game
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="battleShip"></param>
+        public GameBattleShip(IGame game, IBattleShip battleShip)
+        {
+            _game = game;
+            _battleShip = battleShip;
+            _state = BattleShipState.Full;
+        }
+
+        #endregion
+
+    }
+}
